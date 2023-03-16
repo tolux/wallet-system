7aa15af6234e1048520d527233c389f8aeaec81e
tolux <toluandfemi@gmail.com>
2023-03-16 10:53:13 +0100

    intial commit

---
+++ src/user/user.entity.ts
@@ -0,0 +1,27 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { Exclude } from 'class-transformer';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  @Exclude()
+  password: string;
+
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+
+  @Column()
+  address: string;
+
+  @Column()
+  role: string;
+}
